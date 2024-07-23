b36f85922078a90b37bf0445bae27e7f55c05d9d
Sayeed <sali1@ufl.edu>
2024-07-23 15:21:07 -0400

    Real Code

--- sga_lab_9.py
+++ sga_lab_9.py
@@ -1,1 +1,11 @@
-print("Hello")
+def encode(password):
+    encode_list = ''
+    for i in password:
+        i = int(i)
+        x = i + 3
+        if x >= 10:
+            x = x % 10
+        encode_list += x
+    return encode_list
+
+def decode(password):
