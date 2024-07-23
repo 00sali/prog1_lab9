ca128113a3c28ae6cb2dc694ee77b8123a7a0288
Cole Manning <cole.manning@ufl.edu>
2024-07-23 15:28:17 -0400

    Added Decode

--- sga_lab_9.py
+++ sga_lab_9.py
@@ -1,11 +1,18 @@
 def encode(password):
     encode_list = ''
     for i in password:
-        i = int(i)
-        x = i + 3
+        x = int(i) + 3
         if x >= 10:
             x = x % 10
-        encode_list += x
+        encode_list += str(x)
     return encode_list
 
 def decode(password):
+    decodedPass = ""
+    for i in password:
+        x = int(i) - 3
+        if x < 0:
+            x = x + 10
+        decodedPass += str(x)
+    return decodedPass
+
